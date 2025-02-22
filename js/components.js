cbcf1dcf36496cbd0825c61bd1f634fb7ad3d1fc
Wesley Vaz <72579226+wesleydev27@users.noreply.github.com>
2025-02-22 17:50:08 -0300

    general

--- js/components.js
+++ js/components.js
@@ -3,17 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const isSubPage = window.location.pathname.includes('/pages/');
     const basePath = isSubPage ? '../' : './';
 
-    // Carregar o Header
-    fetch(`${basePath}components/header.html`)
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('header').innerHTML = data;
-            // Ajusta os links do header baseado na localização atual
-            adjustHeaderLinks();
-            // Inicializar menu mobile após carregar o header
-            initializeMobileMenu();
-        })
-        .catch(error => console.error('Erro ao carregar o header:', error));
+
 
     // Carregar o Footer
     fetch(`${basePath}components/footer.html`)
@@ -64,3 +54,37 @@ let menu = document.querySelector('#menu');
 btnMob.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
+
+
+// SCRIPT VOLTA PARA O TOPO - Usando `defer` e melhorias de performance
+const backToTopBtn = document.getElementById('back-to-top-btn');
+let ticking = false;
+
+function checkScroll() {
+    if (!ticking) {
+        requestAnimationFrame(() => {
+            const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+            const scrollPosition = window.pageYOffset;
+
+            // Controle de opacidade do botão
+            backToTopBtn.classList.toggle('opacity-0', scrollPosition <= totalHeight / 2);
+            backToTopBtn.classList.toggle('opacity-100', scrollPosition > totalHeight / 2);
+
+            ticking = false;
+        });
+        ticking = true;
+    }
+}
+
+// Função para voltar ao topo da página com animação suave
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
+
+// Adiciona o evento de scroll para controlar a visibilidade do botão
+window.addEventListener('scroll', checkScroll, { passive: true });
+// Adiciona o evento de clique para voltar ao topo
+backToTopBtn.addEventListener('click', scrollToTop);
